b0430bd0a719ef66d17d44e485eeb28a034afdc9
pmint <pmint@cpan.org>
2008-07-01 19:30:53 +0000

    Import of PMINT/HTML-WidgetValidator-Widget-PixivEmbedFeature-0.03 from CPAN.
    gitpan-cpan-distribution: HTML-WidgetValidator-Widget-PixivEmbedFeature
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         PMINT/HTML-WidgetValidator-Widget-PixivEmbedFeature-0.03.tar.gz
    gitpan-cpan-author:       PMINT
    gitpan-cpan-maturity:     released

--- lib/HTML/WidgetValidator/Widget/PixivEmbedFeature.pm
+++ lib/HTML/WidgetValidator/Widget/PixivEmbedFeature.pm
@@ -3,7 +3,7 @@ use base qw(HTML::WidgetValidator::Widget);
 use strict;
 use warnings;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 __PACKAGE__->name('PixivEmbedFeature');
 __PACKAGE__->url('http://www.pixiv.net/');
